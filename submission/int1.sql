f7e9c291120334c181c09b5491c95976f3059455
André Ávila <avila_andre14@hotmail.com>
2022-01-26 19:04:13 +0000

    Modified query number 1.

--- submission/int1.sql
+++ submission/int1.sql
@@ -1,4 +1,4 @@
 .mode columns
 .headers on
 .nullvalue NULL
-select name, sum(CASE WHEN gold <> 0 THEN gold ELSE 0 END) as gold, sum(CASE WHEN silver <> 0 THEN silver ELSE 0 END) as silver, sum(CASE WHEN bronze <> 0 THEN bronze ELSE 0 END) as bronze from Participant, Medal, AthleteEarnsMedal WHERE Participant.idParticipant = AthleteEarnsMedal.idParticipant AND  AthleteEarnsMedal.idMedal = Medal.idMedal GROUP BY Participant.idParticipant ORDER BY sum(CASE WHEN gold <> 0 THEN gold ELSE 0 END) DESC, sum(CASE WHEN silver <> 0 THEN silver ELSE 0 END) DESC, sum(CASE WHEN bronze <> 0 THEN bronze else 0 END) DESC;
+select name, sum(CASE WHEN gold <> 0 THEN gold ELSE 0 END) as gold, sum(CASE WHEN silver <> 0 THEN silver ELSE 0 END) as silver, sum(CASE WHEN bronze <> 0 THEN bronze ELSE 0 END) as bronze from Participant, Medal, AthleteEarnsMedal WHERE Participant.idParticipant = AthleteEarnsMedal.idParticipant AND  AthleteEarnsMedal.idMedal = Medal.idMedal GROUP BY Participant.idParticipant ORDER BY sum(CASE WHEN gold <> 0 THEN gold ELSE 0 END) DESC, sum(CASE WHEN silver <> 0 THEN silver ELSE 0 END) DESC, sum(CASE WHEN bronze <> 0 THEN bronze else 0 END) DESC LIMIT 10;
